153f2e2fa5df7760404d113955c755e1857a543f
tai104402-svg <tai104402@st.vimaru.edu.vn>
2026-01-07 15:42:47 +0700

    g

--- app/models/RevenueModel.php
+++ app/models/RevenueModel.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../config/Database.php';
+require_once __DIR__ . '/../../config/database.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
