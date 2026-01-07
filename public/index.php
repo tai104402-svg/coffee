4556e6f2d2fa84d70687a930b1a11d5773958b45
tai104402-svg <tai104402@st.vimaru.edu.vn>
2026-01-07 16:45:11 +0700

    g

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
-
 <?php
+require_once __DIR__ . '/../core/bootstrap.php';
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require_once __DIR__ . '/../core/bootstrap.php';
 // THÊM DÒNG NÀY ĐỂ LOAD THƯ VIỆN
 //require_once __DIR__ . '/../vendor/autoload.php';
 
